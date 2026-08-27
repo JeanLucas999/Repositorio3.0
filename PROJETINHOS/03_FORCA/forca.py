754f1396fcd4e9c4b8bd72fd134df2e70e44defb
JeanLucas999 <jeanlucasgamer1@gmail.com>
2026-08-27 16:50:37 -0300

    forca atualizada

--- PROJETINHOS/03_FORCA/forca.py
+++ PROJETINHOS/03_FORCA/forca.py
@@ -1,23 +1,30 @@
 import tkinter as tk
-class Palavra:
+AzulClaro = '#C3EBEE'
+AzulMedio = "#77A8AC"
+AzulEscuro = "#090E2B"
+
+class EscolherPalavra:
     def __init__(self):
         self.visibilidade = False
-        self.frame = tk.Frame(menu)
+        self.frame = tk.Frame(menu, bg=AzulClaro)
 
     def colocarBotao(self):
-        self.botaoPalavra = tk.Entry(self.frame, width=45, justify='center', font=fonte, show='*')
-        self.botaoPalavra.place(x=75, y=125)
+        self.botaoPalavra = tk.Entry(self.frame, width=50, justify='center', font=fonte, show='*', fg=AzulEscuro)
+        self.botaoPalavra.place(x=75, y=200)
 
-        self.titulo = tk.Label(self.frame, text='SELECIONE A PALAVRA', width=45, justify='center', font=fonte)
-        self.titulo.place(x=75, y=80)
+        self.titulo = tk.Label(self.frame, text='SELECIONE A PALAVRA', width=23, justify='center', font=fonte, bg=AzulMedio, fg=AzulEscuro)
+        self.titulo.place(x=217, y=90)
 
-        self.visivel = tk.Button(self.frame, width=12, justify='center', font=fontepequena, text='VER PALAVRA', command= lambda: self.verSenha())
-        self.visivel.place(x=290, y=175)
+        self.visivel = tk.Button(self.frame, width=15, justify='center', font=fontePequena, text='VER PALAVRA', bg=AzulMedio, fg=AzulEscuro, command= lambda: self.verSenha())
+        self.visivel.place(x=300, y=250)
 
-        self.confirmar = tk.Button(self.frame, width=12, justify='center', font=fonte, text='CONFIRMAR', command= lambda: self.confirmarPalavra())
-        self.confirmar.place(x=268, y=225)
+        self.confirmar = tk.Button(self.frame, width=15, justify='center', font=fonte, text='CONFIRMAR', bg=AzulMedio, fg=AzulEscuro, command= lambda: self.confirmarPalavra())
+        self.confirmar.place(x=264, y=300)
 
-        self.frame.place(x=0, y=0, width=700, height=350)
+        self.fechar = tk.Button(menu, width=3, justify='center', font=fontePequena, text='X', bg=AzulEscuro, fg=AzulClaro, command= lambda: menu.destroy())
+        self.fechar.place(x=650, y=20)
+
+        self.frame.place(x=0, y=0, width=700, height=500)
 
 
     def verSenha(self):
@@ -31,31 +38,51 @@ class Palavra:
             self.visibilidade = False
 
     def confirmarPalavra(self):
-        global palavraget
+        global jogo
         print ('getTxt')
-        palavraget = self.botaoPalavra.get()
+        palavraget = (((self.botaoPalavra.get()).strip()).upper())
+        print (palavraget)
         self.frame.destroy()
+        jogo = Jogo(palavraget)
+        jogo.textoTela()
+        self.fechar.lift()
 
 class Jogo:
-    def __init__(self):
+    def __init__(self, palavra):
+        print (f'Oi jogo, {palavra}')
         self.vidas = 6
+        self.palavra = palavra
+        self.frame = tk.Frame(menu, bg=AzulClaro)
+
+    def textoTela(self):
+        self.textovidas = tk.Label(self.frame, text=f'{self.vidas}/6 VIDAS', font=fonte, width=10, relief='groove', bg=AzulEscuro, fg=AzulClaro)
+        self.textovidas.place(x=30, y=20)
+
+
+        self.frame.place(x=0, y=0, width=700, height=500)
+
 
     def fimdeJogo(self):
+        if self.vidas == 0:
+            pass
+        else:
+            pass
         pass
+        #botao fechar + destroy menu
 
     def atualizar(self):
+        #palavra ou vida
         pass
 
 fonte= ('Segoe UI', 16)
-fontepequena= ('Segoe UI', 10)
+fontePequena= ('Segoe UI', 10)
 
 
 menu = tk.Tk()
 menu.title('Jogo da forca')
-menu.geometry('700x350')
+menu.geometry('700x500')
 
+EscolhaPalavra = EscolherPalavra()
+EscolhaPalavra.colocarBotao()
 
-palavra = Palavra()
-palavra.colocarBotao()
-    
 menu.mainloop()
