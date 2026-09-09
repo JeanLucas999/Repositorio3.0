91094455d2f01943a1ba4e5e3bc575c319290e2f
JeanLucas999 <jeanlucasgamer1@gmail.com>
2026-09-09 14:41:51 -0300

    opa

---
+++ 01_PYTHON/CodarWithOOP/DESAFIOS/05Tudao/02RPG/rpg.py
@@ -0,0 +1,17 @@
+class Combatente:
+    def __init__(self):
+        self.vida = 10
+        self.dano = 1
+
+    def curar():
+        pass
+
+    def atacar():
+        pass
+
+    def defender():
+        pass
+
+class Protagonista(Combatente):
+    def __init__(self):
+        super().__init__()
