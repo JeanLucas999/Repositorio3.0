010135bbaf735bdffeb0696d5b47610ad193224c
JeanLucas999 <jeanlucasgamer1@gmail.com>
2026-09-09 19:40:23 -0300

    oi

---
+++ 01_PYTHON/Desenferrujar?/curso.py
@@ -0,0 +1,7 @@
+nu1 = int(input('digite o primeiro numero: '))
+nu2 = int(input('digite o segundo numero'))
+
+print(f'{nu1+nu2} eh a soma deles')
+print(f'{nu1-nu2} eh a subtracao deles')
+print(f'{nu1*nu2} eh a multiplicao deles')
+print(f'{nu1/nu2} eh a divisao deles')
