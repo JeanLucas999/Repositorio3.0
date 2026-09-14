e5cd2eeda155cd0bc28e16dd619c52516347c34e
JeanLucas999 <jeanlucasgamer1@gmail.com>
2026-09-14 18:18:32 -0300

    oi

--- 01_PYTHON/CodarWithOOP/DESAFIOS/05TUDAO/02RPG/__main__.py
+++ 01_PYTHON/CodarWithOOP/DESAFIOS/05TUDAO/02RPG/__main__.py
@@ -1,5 +1,27 @@
-def main():
+from rpg import *
+
+def iniciarJogo():
+    global Prota
+    nomeProta = str(input('DIGITE O NOME DO SEU PROTAGONISTA: '))
+    jogo = Jogo(nomeProta)
+
+#ACAO DO PLAYER E INIMIGO
+def actionPlayer():
+    #MENU
     pass
+
+def actionEnemie():
+    #COMENTAR O ACONTECIDO
+    pass
+
+def main():
+    iniciarJogo()
+    Inimigo1 = Inimigo()
+    print(Inimigo1.vida)
+    print(Inimigo1.nome)
+    Inimigo1.atacar()
+
+
 
 if __name__ == '__main__':
     main()
